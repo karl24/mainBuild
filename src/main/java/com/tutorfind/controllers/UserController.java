cc3c7751e5e2aed40f3711609b7247957e0703fa
Karl Fernando <karl24fernando@gmail.com>
2018-03-02 20:41:29 -0800

    add userController

--- src/main/java/com/tutorfind/controllers/UserController.java
+++ src/main/java/com/tutorfind/controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.tutorfind;
+package com.tutorfind.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
