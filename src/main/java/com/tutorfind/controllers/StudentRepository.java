229563309a517c090121464cc78add6c6f34573a
Karl Fernando <karl24fernando@gmail.com>
2018-03-16 20:21:34 -0700

    fixing post for student

--- src/main/java/com/tutorfind/controllers/StudentRepository.java
+++ src/main/java/com/tutorfind/controllers/StudentRepository.java
@@ -2,7 +2,9 @@ package com.tutorfind.controllers;
 
 import com.tutorfind.model.StudentDataModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends CrudRepository<StudentDataModel,Integer> {
 
 
