80c665b03991edfddb281939477dbee5414a7dd7
Adam Hardy <fafnix@gmail.com>
2018-03-26 19:25:40 -0700

    adding post controller

---
+++ src/main/java/com/tutorfind/controllers/PostController.java
@@ -0,0 +1,56 @@
+/*
+Created by Adam Hardy based on Karl's StudentController and UserController
+ */
+
+package com.tutorfind.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tutorfind.model.PostDataModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+//import java.security.SecureRandom;
+import java.sql.*;
+import java.util.ArrayList;
+//import java.util.Random;
+
+@RestController
+public abstract class PostController {
+
+    @Autowired
+    private DataSource dataSource;
+
+
+//    private static final Random RANDOM = new SecureRandom();
+
+    public ArrayList<PostDataModel> getActiveUsersFromDB() {
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM post WHERE active IS TRUE ORDER BY createdTs DESC");
+
+            ArrayList<PostDataModel> output = new ArrayList();
+
+            while (rs.next()) {
+                output.add(new PostDataModel(rs.getInt("postId"), rs.getString("posterType"),
+                        rs.getInt("ownerId"), rs.getInt("subjectId"),
+                        rs.getString("location"), rs.getString("availability"),
+                        rs.getBoolean("acceptsPaid"), rs.getDouble("rate"),
+                        rs.getString("unit"), rs.getTimestamp("createdTs"),
+                        rs.getBoolean("active"), rs.getBoolean("acceptsGroupTutoring"),
+                        rs.getInt("currentlySignedUp")));
+            }
+
+            return output;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+
+        }
+    }
+}
