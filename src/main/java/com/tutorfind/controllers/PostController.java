2a53a9a2c27cbefb156860098a38f172ff7d554a
Adam Hardy <fafnix@gmail.com>
2018-03-27 18:18:59 -0700

    attempt 3 on combined get with requestparam

--- src/main/java/com/tutorfind/controllers/PostController.java
+++ src/main/java/com/tutorfind/controllers/PostController.java
@@ -87,7 +87,7 @@ public class PostController{
 
     private ArrayList<PostDataModel> getActivePostsByTypeFromDB(String posterType) {
         try (Connection connection = dataSource.getConnection()) {
-            String query = "SELECT * FROM posts WHERE active IS TRUE AND posterType = ? ORDER BY createdTs DESC";
+            String query = "SELECT * FROM posts WHERE active IS TRUE AND posterType = 'tutor' ORDER BY createdTs DESC";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, posterType);
 
