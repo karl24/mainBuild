0cf2efd7f0d0170290f85c60ef4ffc093fbf3af4
Karl Fernando <karl24fernando@gmail.com>
2018-03-30 13:30:38 -0700

    fixing passhash for student/tutor

--- src/main/java/com/tutorfind/controllers/UserController.java
+++ src/main/java/com/tutorfind/controllers/UserController.java
@@ -47,39 +47,6 @@ public abstract class UserController {
         }
     }
 
-    public String getPassHash(String password){
-
-        String generatedPassword = null;
-        try {
-            // Create MessageDigest instance for MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            //Add password bytes to digest
-            md.update(password.getBytes());
-            //Get the hash's bytes
-            byte[] bytes = md.digest();
-            //This bytes[] has bytes in decimal format;
-            //Convert it to hexadecimal format
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            //Get complete hashed password in hex format
-            generatedPassword = sb.toString();
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            e.printStackTrace();
-        }
-        return generatedPassword;
-    }
-
-    public static byte[] getNextSalt() {
-        byte[] salt = new byte[16];
-        RANDOM.nextBytes(salt);
-        return salt;
-    }
-
 
 
     public void insertUserIntoDB(int userId, String  userName, String email, String salt, String passhash, String userType){
