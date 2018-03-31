06f46521cb4d104ca81398d1c62fcbe82ad6ed14
Karl Fernando <karl24fernando@gmail.com>
2018-03-30 19:49:59 -0700

    student login

--- src/main/java/com/tutorfind/controllers/LoginController.java
+++ src/main/java/com/tutorfind/controllers/LoginController.java
@@ -5,10 +5,7 @@ import com.tutorfind.model.StudentDataModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -18,7 +15,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 
-
+@RestController
+@RequestMapping("login")
 public class LoginController {
 
 
@@ -26,7 +24,7 @@ public class LoginController {
     private DataSource dataSource;
 
 
-    @RequestMapping(value = "login", method = {RequestMethod.POST})
+    @RequestMapping(method = {RequestMethod.POST})
     public StudentDataModel loginStudent(@RequestBody StudentDataModel s){
 
         try (Connection connection = dataSource.getConnection()) {
