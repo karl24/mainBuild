fc1d28d0efc7273938289974f78668a69c20793c
Karl Fernando <karl24fernando@gmail.com>
2018-04-05 19:21:48 -0700

    student login

--- src/main/java/com/tutorfind/controllers/StudentController.java
+++ src/main/java/com/tutorfind/controllers/StudentController.java
@@ -212,9 +212,10 @@ public class StudentController extends UserController{
     public StudentDataModel loginStudent(@RequestBody StudentDataModel s){
         ArrayList<StudentDataModel> students = getStudentsFromDB();
         try (Connection connection = dataSource.getConnection()) {
-            Statement stmt = connection.createStatement();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT userId, username, passhash FROM users WHERE userName = ? AND passhash = crypt('$2a$06$cyFdOCjX9kgUqhLaDg59E.7fXSeUc9i4W2/EjkEVO73NXUlVM3d9u', passhash)");
-            preparedStatement.setString(1,s.getUserName());
+
+            String sql = "SELECT passhash = crypt('$2a$06$cyFdOCjX9kgUqhLaDg59E.7fXSeUc9i4W2/EjkEVO73NXUlVM3d9u', passhash), userId from users";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            //preparedStatement.setString(1,s.getUserName());
            // preparedStatement.setString(2,s.getPasshash());
             ResultSet rs = preparedStatement.executeQuery();
 
