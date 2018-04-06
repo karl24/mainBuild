43e34049088bb4aca8a7bc460e944e6587e9ffc5
Karl Fernando <karl24fernando@gmail.com>
2018-04-05 18:34:31 -0700

    student login

--- src/main/java/com/tutorfind/controllers/StudentController.java
+++ src/main/java/com/tutorfind/controllers/StudentController.java
@@ -1,5 +1,6 @@
 package com.tutorfind.controllers;
 
+import com.tutorfind.exceptions.ResourceNotFoundException;
 import com.tutorfind.model.StudentDataModel;
 import com.tutorfind.model.UserDataModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -207,7 +208,40 @@ public class StudentController extends UserController{
     }
 
 
+    @RequestMapping(value = "login", method = {RequestMethod.POST})
+    public StudentDataModel loginStudent(@RequestBody StudentDataModel s){
+        ArrayList<StudentDataModel> students = getStudentsFromDB();
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT userId, username, passhash FROM users WHERE userName = ?");
+            preparedStatement.setString(1,s.getUserName());
+            ResultSet rs = preparedStatement.executeQuery();
 
+
+            ArrayList<StudentDataModel> output = new ArrayList<StudentDataModel>();
+
+            while (rs.next()) {
+                for(StudentDataModel student : students){
+                    if(rs.getInt("userId") == student.getUserId());
+                    output.add(new StudentDataModel());
+                }
+
+            }
+
+            if(output.isEmpty()){
+                throw new ResourceNotFoundException();
+            }else {
+                System.out.println(output.get(0));
+                return output.get(0);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+
+        }
+
+    }
 
 
 
