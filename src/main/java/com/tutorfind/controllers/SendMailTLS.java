49a5703aea6ac9670308a9f36a788bb1c116cf28
Karl Fernando <karl24fernando@gmail.com>
2018-04-11 01:10:30 -0700

    testing sending email

---
+++ src/main/java/com/tutorfind/controllers/SendMailTLS.java
@@ -0,0 +1,57 @@
+package com.tutorfind.controllers;
+
+import com.tutorfind.model.StudentDataModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@RestController
+@RequestMapping("mail")
+public class SendMailTLS {
+
+    @RequestMapping(method = {RequestMethod.GET})
+    public ResponseEntity<StudentDataModel> sendMail() {
+        final String username = "karl24fernando@gmail.com"; // enter your mail id
+        final String password = "karlfernando24";// enter ur password
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("karl24fernando@gmail.com")); // same email id
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("karl24fernando@gmail.com"));// whome u have to send mails that person id
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
