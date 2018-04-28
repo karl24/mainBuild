158d925c7c139e8665e185c435f6960ea772305e
Karl Fernando <karl24fernando@gmail.com>
2018-04-27 20:56:58 -0700

    student email

--- src/main/java/com/tutorfind/controllers/StudentController.java
+++ src/main/java/com/tutorfind/controllers/StudentController.java
@@ -243,11 +243,16 @@ public class StudentController extends UserController{
 
     @RequestMapping(value = "{studentId}", method = {RequestMethod.POST})
     public ResponseEntity<StudentDataModel> updateStudent(@PathVariable("studentId") int id, @RequestBody StudentDataModel s) {
+          
+            String domainName = s.getEmail().substring(s.getEmail().length()-17);
+            if(domainName.equals("students.ndnu.edu")) {
+                updateStudentFromDB(id, s.getLegalFirstName(), s.getLegalLastName(), s.getBio(), s.getMajor(), s.getMinor(), s.getImg(), s.isActive());
+                updateUserFromDB(id, s.getUserName(), s.getEmail(), s.getUserType(), s.getSubjects());
+                return new ResponseEntity<>(HttpStatus.OK);
+            }else {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
 
-
-            updateStudentFromDB(id,s.getLegalFirstName(),s.getLegalLastName(),s.getBio(),s.getMajor(),s.getMinor(),s.getImg(),s.isActive());
-            updateUserFromDB(id,s.getUserName(),s.getEmail(),s.getUserType(),s.getSubjects());
-            return new ResponseEntity<>(HttpStatus.OK);
 
 
     }
